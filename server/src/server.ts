90ca963068ca26589a9369088b66a2bf0540e4f6
maarcusvinicius <marcus.editor77@gmail.com>
2022-11-01 18:50:46 -0300

    init project

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import Fastify from 'fastify';
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ['query']
+})
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true
+  })
+
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
+
+    return { count }
+  })
+
+  await fastify.listen({ port:3333 })
+}
+
+bootstrap()
