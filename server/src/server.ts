637df6e6a8422eb28164006ce5c5aa938fe8bacf
maarcusvinicius <marcus.editor77@gmail.com>
2022-11-01 22:10:22 -0300

    /* host

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     return { count }
   })
 
-  await fastify.listen({ port:3333, host: '0.0.0.0' })
+  await fastify.listen({ port:3333, /*host: '0.0.0.0'*/ })
 }
 
 bootstrap()
